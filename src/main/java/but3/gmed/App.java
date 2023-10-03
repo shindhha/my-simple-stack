a96946bc7670deb2caef8261eab8fe1bc3871f86
GUILLAUME MEDARD <guillaume.medard@iut.local>
2023-10-03 16:10:33 +0200

    First commit

---
+++ src/main/java/but3/gmed/App.java
@@ -0,0 +1,13 @@
+package but3.gmed;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/but3/gmed/AppTest.java
@@ -0,0 +1,20 @@
+package but3.gmed;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
