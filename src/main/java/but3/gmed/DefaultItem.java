c1b00316f02b3ed05ae6c903870546ca6b667dba
GUILLAUME MEDARD <guillaume.medard@iut.local>
2023-10-10 16:08:55 +0200

    Ajout de la référence vers les SimpleStack

--- src/main/java/but3/gmed/DefaultItem.java
+++ src/main/java/but3/gmed/DefaultItem.java
@@ -3,7 +3,7 @@ package but3.gmed;
 /**
  * Ceci est un objet par défaut.
  * Cet objet et par défaut destiné a être utilisé avec les "SimpleStack"
- *
+ * @see SimpleStack
  */
 public class DefaultItem implements Item {
 
