ebbbda741d7a744e5d15855f398e895707b4cd5a
GUILLAUME MEDARD <guillaume.medard@iut.local>
2023-10-10 16:08:21 +0200

    Contexte des defaultItem

--- src/main/java/but3/gmed/DefaultItem.java
+++ src/main/java/but3/gmed/DefaultItem.java
@@ -1,8 +1,8 @@
 package but3.gmed;
 
 /**
- * Ceci est un objet par défaut
- *
+ * Ceci est un objet par défaut.
+ * Cet objet et par défaut destiné a être utilisé avec les "SimpleStack"
  *
  */
 public class DefaultItem implements Item {
